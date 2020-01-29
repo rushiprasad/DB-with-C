3ee1107181e00754c7b3e5c54e7a124e481dd2f4
rushiprasad <rushinarwade353@gmail.com>
2020-01-29 08:56:01 +0530

    Update db.c

--- db.c
+++ db.c
@@ -3,18 +3,19 @@
 #include<stdlib.h>
 int main()
 {
-    int col, rows;
+    int col, col1, rows;
     char tname[30], colname[20], data[20];
     printf("Enter the Table Name >");
     scanf("%s", &tname);
 
     strcat(tname, ".txt");
 
-    FILE * tablename =fopen(tname, "a");
+    FILE * tablename =fopen(tname, "w");
 
     printf("Enter the number of colums: ");
     scanf("%d", &col);
     printf("\n");
+    col1=col;
     while(col)
     {
         printf("Enter the column name >");
@@ -23,22 +24,25 @@ int main()
         fprintf(tablename, "%15s", colname);
         col--;
     }
+    fprintf(tablename,"\n");
     printf("\nTable Created Successfully...");
 
     printf("\n\nNow Insert the Data ");
    
     printf("\n\nHow many rows??");
     scanf("%d", &rows);
-    while(rows+2)
+    while(rows)
     {
-        printf("Enter the data > ");
-        scanf("%s", &data);
-        // fprintf(tablename, "\n", stdin);
-        fprintf(tablename, "%10s\t", data);
-
+    	for(col=col1;col;col--)
+    	{
+        	printf("Enter the data > ");
+	        scanf("%s", &data);
+    	    // fprintf(tablename, "\n", stdin);
+        	fprintf(tablename, "%15s\t", data);
+		}
+		fprintf(tablename, "\n", data);
         rows--;
     }
     printf("Your data was inserted successfully");
 
 }
-
